9069e97c007e28e60e09d9d66e214b5badd59d1b
Koushik Dutta <koushd@gmail.com>
2019-12-01 11:18:10 -0800

    conscrypt is optional

--- src/jvmMain/kotlin/com/koushikdutta/scratch/http/client/middleware/actual.kt
+++ src/jvmMain/kotlin/com/koushikdutta/scratch/http/client/middleware/actual.kt
@@ -3,5 +3,10 @@ package com.koushikdutta.scratch.http.client.middleware
 import com.koushikdutta.scratch.http.client.AsyncHttpClient
 
 actual fun AsyncHttpClient.addPlatformMiddleware(client: AsyncHttpClient) {
-    client.middlewares.add(ConscryptMiddleware(client.eventLoop))
+    try {
+        client.middlewares.add(ConscryptMiddleware(client.eventLoop))
+    }
+    catch (throwable: Throwable) {
+        System.err.println("failed to load conscrypt")
+    }
 }
