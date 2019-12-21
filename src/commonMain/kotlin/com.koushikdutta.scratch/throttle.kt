453efa13abfa694573ce885cbe1ba358aeccb0c1
Koushik Dutta <koushd@gmail.com>
2019-12-20 17:47:23 -0800

    throttle

---
+++ src/commonMain/kotlin/com.koushikdutta.scratch/throttle.kt
@@ -0,0 +1,26 @@
+package com.koushikdutta.scratch
+
+import com.koushikdutta.scratch.event.AsyncEventLoop
+import com.koushikdutta.scratch.event.Cancellable
+
+typealias ValuesCallback<T> = (values: Collection<T>) -> Unit
+
+class Throttle<T>(val loop: AsyncEventLoop, val milliseconds: Int, private val callback: ValuesCallback<T>) {
+    val values = mutableListOf<T>()
+    var cancellable: Cancellable? = null
+    private fun runQueue() {
+        cancellable = null
+        val copy = mutableListOf<T>()
+        copy.addAll(values)
+        values.clear()
+        callback(copy)
+    }
+
+    fun post(value: T) {
+        loop.post {
+            values.add(value)
+            if (cancellable == null)
+                cancellable = loop.postDelayed(milliseconds.toLong(), this::runQueue)
+        }
+    }
+}
